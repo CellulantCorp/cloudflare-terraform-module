a88333ecbea4c7530fb65051283e2ea80da5cb63
Xavier Were <20048168+masterxavierfox@users.noreply.github.com>
2021-05-11 13:40:08 +0300

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,8 +1,13 @@
 terraform {
   required_providers {
     cloudflare = {
-      source  = "cloudflare/cloudflare"
+      source = "cloudflare/cloudflare"
       version = "2.20.0"
     }
   }
+}
+
+provider "cloudflare" {
+  # Configuration options
+  api_token = var.api_token
 }
